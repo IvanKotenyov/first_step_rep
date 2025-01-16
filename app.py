52f80cc60b0e8dae6916ee50d11732969b0d33e5
Иван Котенёв <@step.krg>
2025-01-16 21:21:30 +0500

    second commit

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-from flask import(
-    Flask,
-    render_template,
-    request,
-    redirect
-)
-app = Flask(__name__)
+# from flask import(
+#     Flask,
+#     render_template,
+#     request,
+#     redirect
+# )
+# app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def get_base():
-    return render_template('base.html')
+# @app.route('/', methods=['GET','POST'])
+# def get_base():
+#     return render_template('base.html')
 
-@app.route('/reg', methods=['GET','POST'])
-def get_reg():
-    return render_template('reg.html')
+# @app.route('/reg', methods=['GET','POST'])
+# def get_reg():
+#     return render_template('reg.html')
 
-@app.route('/log', methods=['GET','POST'])
-def get3_log():
-    return render_template('log.html')
+# @app.route('/log', methods=['GET','POST'])
+# def get3_log():
+#     return render_template('log.html')
 
 
-if __name__ == '__main__':
-    app.run(
-        debug=True,
-        port=8080
-    )
+# if __name__ == '__main__':
+#     app.run(
+#         debug=True,
+#         port=8080
+#     )
