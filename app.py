a4e9477660023167d61e8607e30f188292b68ad6
Иван Котенёв <@step.krg>
2025-01-16 20:44:56 +0500

    first commit rudge

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import(
+    Flask,
+    render_template,
+    request,
+    redirect
+)
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def get_base():
+    return render_template('base.html')
+
+@app.route('/reg', methods=['GET','POST'])
+def get_reg():
+    return render_template('reg.html')
+
+@app.route('/log', methods=['GET','POST'])
+def get3_log():
+    return render_template('log.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=8080
+    )
